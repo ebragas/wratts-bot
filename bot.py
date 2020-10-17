12451ee84f0c30a178a099025bc11ea550ace0ea
Eric Bragas <eric@flywheelsoftware.com>
2020-10-17 09:49:25 -0700

    things

--- bot.py
+++ bot.py
@@ -4,16 +4,29 @@ import os
 import discord
 from dotenv import load_dotenv
 
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
+
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    guild = discord.utils.get(client.guilds, name=GUILD)
+    print(
+        f'{client.user} has connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})\n'
+    )
 
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
 
-if __name__ == '__main__':
-    load_dotenv()
-    TOKEN = os.getenv('DISCORD_TOKEN')
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
 
-    client = discord.Client()
-
-    client.run(TOKEN)
-
+client.run(TOKEN)
