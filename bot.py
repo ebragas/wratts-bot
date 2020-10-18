60b34cfce63b2e508c87b4486cc1facbec0e85d4
Eric Bragas <eric@flywheelsoftware.com>
2020-10-18 14:41:37 -0700

    move to subclass structure

--- bot.py
+++ bot.py
@@ -5,28 +5,39 @@ import discord
 from dotenv import load_dotenv
 
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+class WrattBot(discord.Client):
+    async def on_ready(self):
+        guild = discord.utils.get(client.guilds, name=GUILD)
+        print(
+            f'{client.user} has connected to the following guild:\n'
+            f'{guild.name}(id: {guild.id})\n'
+        )
 
-@client.event
-async def on_ready():
-    guild = discord.utils.get(client.guilds, name=GUILD)
-    print(
-        f'{client.user} has connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})\n'
-    )
+        members = '\n - '.join([member.name for member in guild.members])
+        print(f'Guild Members:\n - {members}')
 
-    members = '\n - '.join([member.name for member in guild.members])
-    print(f'Guild Members:\n - {members}')
+    async def on_member_join(self, member):
+        logging.info('on_member_join')
+        channel = client.get_channel(ID)
+        await channel.edit(name = 'Member count: {}'.format(channel.guild.member_count))
 
-@client.event
-async def on_member_join(member):
-    await member.create_dm()
-    await member.dm_channel.send(
-        f'Hi {member.name}, welcome to my Discord server!'
-    )
+    # async def on_member_join(self, member):
+    #     logging.info('on_member_join')
+    #     await member.create_dm()
+    #     await member.dm_channel.send(
+    #         f'Hi {member.name}, welcome to my Discord server!'
+    #     )
 
-client.run(TOKEN)
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+
+if __name__ == '__main__':
+
+    load_dotenv()
+    TOKEN = os.getenv('DISCORD_TOKEN')
+    GUILD = os.getenv('DISCORD_GUILD')
+
+    client = WrattBot()
+    client.run(TOKEN)
