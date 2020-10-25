542a19ea141ed094622cc964f464b58a24583835
Eric Bragas <eric@flywheelsoftware.com>
2020-10-25 14:46:00 -0700

    moar formatting

--- bot.py
+++ bot.py
@@ -4,9 +4,9 @@ import logging
 import os
 import random
 
-import discord
 from discord.ext import commands
 from dotenv import load_dotenv # NOTE: not sure I want to keep this
+import discord
 
 
 bot = commands.Bot(command_prefix='!')
@@ -14,17 +14,19 @@ bot = commands.Bot(command_prefix='!')
 
 @bot.event
 async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
+    """Logs bot ready state"""
+    logging.info(f'{bot.user.name} has connected to Discord!')
 
 
 @bot.command(name='99', help='Responds with a random Brooklyn Nine Nine quote')
 async def nine_nine(ctx):
+    """Responds with a random Brooklyn Nine Nine quote"""
     b99_quotes = [
         "I'm the human form of the :100: emoji.",
         'Bingpot!',
         (
             'Cool. Cool cool cool cool cool cool cool cool, '
-            'no doubt no doubt no doubt no doubt.'
+            + 'no doubt no doubt no doubt no doubt.'
         ),
     ]
 
@@ -34,6 +36,7 @@ async def nine_nine(ctx):
 
 @bot.command(name='roll', help='Rolls a die a few times and returns the results')
 async def roll_dice(ctx, times: int, sides: int):
+    """Rolls virtual dice"""
     rolls = [
         str(random.choice(range(1, sides + 1)))
         for _ in range(times)
